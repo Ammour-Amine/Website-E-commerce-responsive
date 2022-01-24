56ec11357a65d4d076fe09000d79b1ec1c5257e7
Amine Ammour <amineammour4@gmail.com>
2022-01-25 00:41:47 +0100

    slide

--- javascript/main.js
+++ javascript/main.js
@@ -5,3 +5,20 @@ menu.addEventListener("click",()=>{
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 });
+
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let slides = document.querySelectorAll(".slide-container");
+let index = 0;
+
+next.addEventListener("click",()=>{
+    slides[index].classList.remove("active");
+    index =(index +1) % slides.length;
+    slides[index].classList.add("active");
+});
+
+prev.addEventListener("click",()=>{
+    slides[index].classList.remove("active");
+    index =(index - 1 + slides.length) % slides.length;
+    slides[index].classList.add("active");
+});
